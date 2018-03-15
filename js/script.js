e967c8ef6f3760501af980f4bfaa094a194a0bd4
Karol Dąbrowski <dabrowski.carl@gmail.com>
2018-03-15 23:15:14 +0100

    Add files, index sctructure and triangle scritp

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var a = prompt('enter first value'),
+    h = prompt('enter second value'),
+    triangleArea = a*h/2;
+alert('Your triangle area is:' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
